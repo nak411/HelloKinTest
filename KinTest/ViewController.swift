1c30c03973960fc205697ae227798c8ae5dbc7d4
Naveed <nak411@gmail.com>
2019-08-13 22:23:04 -0400

    Initial Commit

---
+++ KinTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KinTest
+//
+//  Created by Naveed on 8/13/19.
+//  Copyright © 2019 Naveed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
